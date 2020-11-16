64b1b983b9b428bc8e51e20bd67b055d06d60c87
Ignacio <ilopez-amor@ya.com>
2020-11-16 19:16:54 +0100

    Created for only for tests

---
+++ app/views/first/Fake.vue
@@ -0,0 +1,43 @@
+<template>
+  <Page class="page">
+    <ActionBar
+      title="Home"
+      class="action-bar"
+    />
+    <ScrollView>
+      <StackLayout class="home-panel">
+        <!--Add your page content here-->
+        <Label
+          textWrap="true"
+          text="FAKE PAGE!!!"
+          class="h2 description-label"
+          color="black"
+        />
+      </StackLayout>
+    </ScrollView>
+  </Page>
+</template>
+
+<script>
+export default {
+    name: 'Fake',
+    data () {
+        return {
+        };
+    },
+}
+</script>
+
+<style scoped>
+.home-panel {
+    vertical-align: center;
+    font-size: 20;
+    margin: 15;
+    color: black;
+}
+
+.description-label {
+    margin: 15;
+}
+</style>
+
