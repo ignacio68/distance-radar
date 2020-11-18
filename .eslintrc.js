34b9c050614f5d9da28a1faed4d2aa0b63340d06
Ignacio <ilopez-amor@ya.com>
2020-11-18 10:53:20 +0100

    Added typescript linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-    es6: true,
-    jest: true
-  },
-  plugins: [
-    "vue"
-  ],
-  extends: [
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/recommended',
-    'eslint:recommended',
-    // '@vue/typescript/recommended',
-    // "prettier/vue",
-    // 'plugin:prettier/recommended',
-  ],
-  // required to lint *.vue files
-  // plugins: ['prettier'],
-  //   plugins: ['vue', 'prettier'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'space-before-function-paren': 0,
-    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-    'vue/attribute-hyphenation': [
-      'error',
-      'never',
-      { ignore: ['custom-prop', 'v-'] }
-    ]
-    // 'no-unused-vars': ["error", {"args": "none"}],
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-    ecmaFeatures: {
-      legacyDecorators: true
+    root: true,
+    env: {
+        node: true,
+        browser: true,
+        es2020: true,
+        jest: true,
     },
-    sourceType: 'module'
-  },
-  globals: {
-    $nuxt: true
-  }
+    plugins: ['vue', 'prettier', '@typescript-eslint'],
+    extends: [
+        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+        'plugin:vue/recommended',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        // '@vue/typescript/recommended',
+        // "prettier/vue",
+        'plugin:prettier/recommended',
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'space-before-function-paren': 0,
+        'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+        'vue/attribute-hyphenation': [
+            'error',
+            'never',
+            { ignore: ['custom-prop', 'v-'] },
+        ],
+        // 'no-unused-vars': ["error", {"args": "none"}],
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        parser: 'babel-eslint',
+        ecmaFeatures: {
+            legacyDecorators: true,
+        },
+        sourceType: 'module',
+    },
+    globals: {
+        $nuxt: true,
+    },
 }
