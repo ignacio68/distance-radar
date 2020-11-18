3c26ec7e8de98afb78d8e19ee212d16170b500c1
Ignacio <ilopez-amor@ya.com>
2020-11-18 11:08:10 +0100

    Resolved linter/format issues

--- app/store/userMarkerStore.ts
+++ app/store/userMarkerStore.ts
@@ -3,16 +3,17 @@ import Vue from 'nativescript-vue'
 import { UserMarker, LngLat } from '@/types/types'
 
 const state = Vue.observable({
-    userMarker: [],
+  userMarker: [],
 })
 
-export const hasUserMarker = () => state.userMarker.length > 0
+export const hasUserMarker = (): boolean => state.userMarker.length > 0
 
 export const getUserMarker = (): UserMarker => state.userMarker[0]
 
-export const setUserMarker = (marker: UserMarker) => state.userMarker.push(marker)
+export const setUserMarker = (marker: UserMarker): number =>
+  state.userMarker.push(marker)
 
-export const updateUserMarkerPosition = (coordinates: LngLat) => {
-    state.userMarker[0].lat = coordinates.lat
-    state.userMarker[0].lng = coordinates.lng
+export const updateUserMarkerPosition = (coordinates: LngLat): void => {
+  state.userMarker[0].lat = coordinates.lat
+  state.userMarker[0].lng = coordinates.lng
 }
