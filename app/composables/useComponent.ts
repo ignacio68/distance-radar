8cfd65e1ba929b9f6426ea5343d7c6db2e5d8235
Ignacio <ilopez-amor@ya.com>
2020-11-20 13:57:08 +0100

    Added new security area prop

--- app/composables/useComponent.ts
+++ app/composables/useComponent.ts
@@ -3,16 +3,18 @@ import Vue from 'nativescript-vue'
 const isVisible = Vue.observable({
   geocoder: false,
   newLocationMenu: false,
+  newSecurityAreaMenu: false,
   securityArea: false,
   textFieldSoftKeyboard: true,
 })
 
 export const getVisibility = (item: string): boolean => isVisible[item]
 
-export const toggleVisibility = (item: string) =>
+export const toggleVisibility = (item: string): boolean =>
   (isVisible[item] = !isVisible[item])
 
-export const setVisibility = (item: string, visibility: boolean) => isVisible[item] = visibility
+export const setVisibility = (item: string, visibility: boolean): boolean =>
+  (isVisible[item] = visibility)
 
 export default {
   getVisibility,
