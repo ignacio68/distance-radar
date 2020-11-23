5a993367aa2eed793c1c8f992ea43ddfb25cff8e
Ignacio <ilopez-amor@ya.com>
2020-11-23 14:20:44 +0100

    Formatted

--- app/services/geocodingService.ts
+++ app/services/geocodingService.ts
@@ -2,15 +2,15 @@ import * as geocoding from 'nativescript-geocoding'
 
 import { Location } from '@/types/types/geocoder'
 
-type SearchLocationsResult =  void | Location[]
+type SearchLocationsResult = void | Location[]
 
-export const searchLocations = async(query: string): Promise<SearchLocationsResult> => {
-  const fetchLocations = await geocoding
-    .getLocationListFromName(query, 5)
-    .then(
-      (locations: SearchLocationsResult) => locations,
-      (error: any) => console.log(`Error: ${error.message || error}`)
-    )
+export const searchLocations = async (
+  query: string
+): Promise<SearchLocationsResult> => {
+  const fetchLocations = await geocoding.getLocationListFromName(query, 5).then(
+    (locations: SearchLocationsResult) => locations,
+    (error: any) => console.log(`Error: ${error.message || error}`)
+  )
   console.log(`fetchLocations: ${JSON.stringify(fetchLocations)}`)
   return fetchLocations
 }
