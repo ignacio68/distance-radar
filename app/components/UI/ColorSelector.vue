f35c3a5a908987d222fea7c38a180762b2a6a8ff
Ignacio <ilopez-amor@ya.com>
2020-11-23 14:19:28 +0100

    New component

---
+++ app/components/UI/ColorSelector.vue
@@ -0,0 +1,93 @@
+<template>
+  <StackLayout
+    orientation="horizontal"
+    verticalAlignment="middle"
+  >
+    <Label
+      class="color_title"
+      :text="$t('lang.components.colorSelector.text')"
+      :width="labelWidth"
+      textWrap="true"
+    />
+    <StackLayout orientation="horizontal">
+      <Label
+        v-for="color in colors"
+        :key="color.name"
+        ref="colorItem"
+        class="color_list-item"
+        :backgroundColor="color.color"
+        borderColor="#00251e"
+        borderWidth="1"
+        @Tap="onTap(color)"
+      />
+    </StackLayout>
+  </StackLayout>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue'
+
+import { Color, Enums } from '@nativescript/core'
+
+export default Vue.extend({
+  name: 'ColorSelector',
+  props: {
+    labelWidth: {
+      type: [String, Number],
+      default: 80
+    }
+  },
+  data() {
+    return {
+      colors:
+        [
+          { name: 'blue', color: '#31aec4' },
+          { name: 'indigo', color: '#a191ff' },
+          { name: 'violet', color: '#d17bff' },
+          { name: 'orange', color: '#cc9633' },
+          { name: 'red', color: '#ff765d'} ,
+          { name: 'pink', color: '#ff6ea1' }
+        ]
+    }
+  },
+  methods:{
+      onTap(color) {
+        this.colorItemRipple
+        this.$emit('on-selected-color', color)
+        console.log(`color: ${color.name}`)
+      },
+      // FIXME: fix the color ripple animation, doesn't work
+      colorItemRipple() {
+        const colorItem = this.$refs.colorItem.nativeView
+        const currentColor = colorItem.color
+        const newColor = new Color(10, 0, 122, 122)
+        colorItem
+          .animate({
+            curve: Enums.AnimationCurve.linear,
+            duration: 500,
+            color: newColor,
+          })
+          .then(() => {
+            colorItem
+              .animate({
+                curve: colorItem.linear,
+                duration: 500,
+                color: currentColor
+              })
+              .then(() => {})
+          })
+          .catch(() => {})
+      }
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+@import '../../app-variables';
+
+  .color_list-item {
+    margin-left: 8;
+    width: 24;
+    height: 24;
+  }
+</style>>
---
+++ app/components/UI/CustomSlider.vue
@@ -0,0 +1,97 @@
+<template>
+  <GridLayout
+    class="customSlider"
+    columns="100,*"
+    color="black"
+  >
+    <Label
+      col="0"
+      class="customSlider_name"
+      :text="sliderName"
+    />
+    <GridLayout
+      col="1"
+      rows="24, 24"
+      columns="100, *, 100"
+    >
+      <MDSlider
+        v-model="value"
+        col="0"
+        row="0"
+        colSpan="3"
+        class="customSlider_slider"
+        :minValue="minValue"
+        :maxValue="maxValue"
+        :value="value"
+        @selectedIndexChanged="$emit('on-index-changed', value)"
+      />
+      <Label
+        col="0"
+        row="1"
+        class="customSlider_minValue"
+        backgroundColor="blue"
+        :text="minValue"
+      />
+      <!-- <Label
+        col="1"
+        row="1"
+        text="solo estoy para rellenar"
+        backgroundColor="red"
+      /> -->
+      <Label
+        col="2"
+        row="1"
+        class="customSlider_maxValue"
+        backgroundColor="lime"
+        :text="maxValue"
+      />
+    </GridLayout>
+  </GridLayout>
+</template>
+
+<script lang="ts">
+import Vue from 'nativescript-vue'
+
+import '@/plugins/installMDSlider'
+
+export default Vue.extend({
+  name: 'CustomSlider',
+  props: {
+    sliderName: {
+      type: String,
+      default: '',
+    },
+    minValue: {
+      type: Number,
+      default: 0,
+    },
+    maxValue: {
+      type: Number,
+      default: 1,
+    },
+    value: {
+      type: Number,
+      default: 0,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+  .customSlider_name {
+    padding {
+      right: 8;
+    }
+    font-size: 16;
+  }
+  .customSlider_minValue {
+    padding {
+      left: 24;
+    }
+  }
+  .customSlider_maxValue {
+    padding {
+      right: 24;
+    }
+  }
+</style>
