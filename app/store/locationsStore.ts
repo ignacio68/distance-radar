1603ed75ff5335387b6b159e378368a9309f5ad1
Ignacio <ilopez-amor@ya.com>
2020-11-23 14:22:14 +0100

    Formatted

--- app/store/locationsStore.ts
+++ app/store/locationsStore.ts
@@ -3,11 +3,16 @@ import Vue from 'nativescript-vue'
 import { Location } from '@/types/types'
 
 const state = Vue.observable({
-  locations: ([] as unknown) as [Location],
+  locations: ([] as unknown) as Location[],
+  selectedLocation: '',
 })
 
 const findIndex = (id: string): number =>
   state.locations.findIndex((location) => location.id === id)
+
+// const findSelectedLocation: number = state.locations.findIndex(
+//   (location) => location.selected === true
+// )
 
 // export const hasId = (id: string): boolean =>
 //   (findIndex(id) as unknown) as boolean
@@ -17,11 +22,11 @@ export const hasId = (id: string): boolean =>
 export const numberOfLocations = (): number => state.locations.length
 
 export const getLocation = (id: string): Location =>
-  state.locations.find((location) => (location.id = id))
+  state.locations.find((location) => location.id === id)
 
-export const getLocations = state.locations
+export const getLocations = (): Location[] => state.locations
 
-export const addNewLocation = (location: Location): number =>
+export const addNewLocation = async (location: Location): Promise<number> =>
   state.locations.push(location)
 
 // setStorage(location.id, location).then(success => {
@@ -31,15 +36,31 @@ export const addNewLocation = (location: Location): number =>
 //   }
 // })
 
-const updateLocation = (location: Location): Location => {
+const updateLocation = async (location: Location): Promise<Location> => {
   const currentLocation = getLocation(location.id)
-  const updatedLocation = Object.assign(currentLocation, location)
+  const updatedLocation = { ...currentLocation, ...location }
   return updatedLocation
 }
-export const updateLocationsStore = (location: Location): void => {
-  const updatedLocation = updateLocation(location)
-  state.locations.splice(findIndex(location.id), 1, updatedLocation)
+
+export const updateLocationsStore = async (
+  location: Location
+): Promise<void> => {
+  const resolveLocation = await updateLocation(location)
+  state.locations.splice(findIndex(location.id), 1, resolveLocation)
 }
 
 export const deleteLocation = (id: string): Location[] =>
   state.locations.splice(findIndex(id), 1)
+
+export const setSelectedLocation = (id: string): string => {
+  console.log(`locationsStore.ts::setSelectedLocation: ${id}`)
+  return (state.selectedLocation = id)
+}
+
+// export const getSelectedLocation = (): Location => {
+//   const index = findSelectedLocation
+//   return state.locations[index]
+// }
+
+export const getSelectedLocation = (): Location =>
+  getLocation(state.selectedLocation)
