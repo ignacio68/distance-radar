c2e820d56cef7ae7f211fc73a49b1e4d9e49f4d1
Ignacio <ilopez-amor@ya.com>
2020-11-25 10:16:29 +0100

    Added arguments normalization

---
+++ app/utils/circle/normalizations/index.ts
@@ -0,0 +1,9 @@
+import { Circle } from '@/types/types'
+const normalizeArg = (arg: number): number => Math.abs(Math.round(arg))
+
+export const normalizeArgs = async (args: Circle): Promise<Circle> => {
+  const normalizedRadius = normalizeArg(args.radius)
+  const normalizedNumberOfEdges = normalizeArg(args.numberOfEdges)
+  const normalizedArgs = { ...args, normalizedRadius, normalizedNumberOfEdges }
+  return normalizedArgs
+}
