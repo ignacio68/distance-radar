d4d6ed62d001e8bd3218d7a860345bbdc48bdbfa
Ignacio <ilopez-amor@ya.com>
2020-11-25 10:25:06 +0100

    Updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,29 +8,26 @@ module.exports = {
     },
     plugins: ['vue', 'prettier', '@typescript-eslint'],
     extends: [
-        // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-        'plugin:vue/recommended',
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
         // '@vue/typescript/recommended',
-        // "prettier/vue",
         'plugin:prettier/recommended',
+        'plugin:prettier-vue/recommended',
+        'prettier/vue',
+        'plugin:vue/recommended',
     ],
     rules: {
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'space-before-function-paren': 0,
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'space-before-function-paren': 2,
         'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-        'vue/attribute-hyphenation': [
-            'error',
-            'never',
-            { ignore: ['custom-prop', 'v-'] },
-        ],
+        'vue/attribute-hyphenation': ['error', 'never', { ignore: ['custom-prop', 'v-'] }],
         // 'no-unused-vars': ["error", {"args": "none"}],
     },
     parser: '@typescript-eslint/parser',
     parserOptions: {
-        parser: 'babel-eslint',
+        ecmaVersion: 2020,
+        parser: '@babel/eslint-parser',
         ecmaFeatures: {
             legacyDecorators: true,
         },
