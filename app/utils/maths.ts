df0b9f73015ed7404bed7dc10a801c3a3dbd138e
Ignacio <ilopez-amor@ya.com>
2020-11-25 10:19:53 +0100

    Capitalized consts

--- app/utils/maths.ts
+++ app/utils/maths.ts
@@ -1,14 +1,8 @@
-const toRadians = 0.017453292519943295 // (PI / 180)
-const toDegrees = 57.2958 // (180 / PI)
+const TO_RADIANS = 0.017453292519943295 // (PI / 180)
+const TO_DEGREES = 57.2958 // (180 / PI)
 
 export const range = (start: number, stop: number, step: number): number[] =>
   Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
 
-export const degreesToRadians = (degrees: number): number => degrees * toRadians
-export const radiansToDegrees = (radians: number): number => radians * toDegrees
-
-// export {
-//     range,
-//     degreesToRadians,
-//     radiansToDegrees
-// }
+export const toRadians = (degrees: number): number => degrees * TO_RADIANS
+export const toDegrees = (radians: number): number => radians * TO_DEGREES
