73d539e3560a3f949acfc679c816767461a09a8c
Ignacio <ilopez-amor@ya.com>
2020-12-09 11:01:57 +0100

    Added typescript props

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,23 @@
-module.exports = function (api) {
+module.exports = (api) => {
     api.cache(true)
 
     return {
-        presets: [['@babel/env', { targets: { esmodules: true } }]],
+        presets: [
+            ['@babel/preset-env', { targets: { esmodules: true } }],
+            '@babel/preset-typescript',
+        ],
+        env: {
+            test: {
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            targets: { node: 'current' },
+                        },
+                    ],
+                    '@babel/preset-typescript',
+                ],
+            },
+        },
     }
 }
