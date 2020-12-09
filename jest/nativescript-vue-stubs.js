96bbe595e16c6cf57308c484cf54e5eabfe55d50
Ignacio <ilopez-amor@ya.com>
2020-12-09 11:10:43 +0100

    Jest helpers

---
+++ jest/nativescript-vue-stubs.js
@@ -0,0 +1,24 @@
+import { config } from '@vue/test-utils'
+
+const NSElements = [
+    'ActionBar',
+    'ActionItem',
+    // 'CustomActionBar',
+    // 'MapWrapper',
+    // 'BottomAppBar',
+    'ActivityIndicator',
+    'Button',
+    'Frame',
+    'GridLayout',
+    'Image',
+    'Label',
+    'ListView',
+    'Page',
+    'TextField',
+    'ScrollView',
+    'StackLayout',
+]
+
+NSElements.forEach((element) => (config.stubs[element] = true))
+
+// 'MapBox', 'MDFloatingActionButton', 'MDButton', ''
