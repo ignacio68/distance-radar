1e1c6378dfbec75f54a4d5113da176c274f58e0e
Ignacio <ilopez-amor@ya.com>
2020-12-14 13:25:45 +0100

    Redesign the user interface

--- app/components/UI/CustomSlider.vue
+++ app/components/UI/CustomSlider.vue
@@ -1,53 +1,101 @@
 <template>
   <GridLayout
     class="customSlider"
-    columns="100,*"
-    color="black"
+    columns="auto,*"
+    :width="sliderWidth"
   >
-    <Label
+    <Icon
       col="0"
-      class="customSlider_name"
-      :text="sliderName"
+      class="customSlider_icon"
+      :size="iconSize"
+      :name="iconName"
+      :iconColor="iconColor"
+      :hasRipple="false"
     />
-    <GridLayout
+    <!-- <Icon
+      col="0"
+      class="customSlider_icon"
+      v-bind="$props"
+      :hasRipple="false"
+    /> -->
+    <StackLayout
       col="1"
-      rows="auto, auto"
-      columns="100, *, 100"
     >
+      <Label
+        class="customSlider_name"
+        :text="sliderName"
+      />
+      <GridLayout columns="auto, *, auto">
+        <Label
+        col="0"
+        color="black"
+        class="customSlider_minValue"
+        :text="minValue"
+        />
+        <StackLayout
+          col="1"
+          width="100%"
+          color="red"
+        >
+        </StackLayout>
+          <Label
+            col="2"
+            class="customSlider_maxValue"
+            :text="maxValue"
+          />
+      </GridLayout>
       <MDSlider
         :value="value"
         col="0"
         row="0"
         colSpan="3"
-        class="customSlider_slider"
+        class="customSlider_slider m-0"
         :minValue="minValue"
         :maxValue="maxValue"
+        :thumbHollowAtStart="thumbHollowAtStart"
+        :thumbColor="thumbColor"
+        :rippleColor="rippleColor"
+        :elevation="elevation"
         @valueChange="onValueChanged"
       />
-      <Label
-        col="0"
-        row="1"
-        class="customSlider_minValue"
-        :text="minValue"
-      />
-      <Label
-        col="2"
-        row="1"
-        class="customSlider_maxValue"
-        :text="maxValue"
-      />
-    </GridLayout>
+    </StackLayout>
   </GridLayout>
 </template>
 
 <script lang="ts">
 import Vue from 'nativescript-vue'
 
+import Icon from '@/components/UI/Icon.vue'
+
 import '@/plugins/installMDSlider'
 
 export default Vue.extend({
   name: 'CustomSlider',
+  components: { Icon },
   props: {
+    // Icon.$props,
+    sliderWidth: {
+      type: [String, Number],
+      default: '100%'
+    },
+
+    iconName: {
+      type: String,
+      default: ''
+    },
+    iconSize: {
+      type: [String, Number],
+      default: '48'
+    },
+    iconColor: {
+      type: String,
+      default: "black"
+    },
+    rippleColor: {
+      type: String,
+      required: false
+    },
+
     sliderName: {
       type: String,
       default: '',
@@ -59,6 +107,19 @@ export default Vue.extend({
     maxValue: {
       type: Number,
       default: 1,
+    },
+    elevation: {
+      type: [String, Number],
+      default: '5'
+    },
+    thumbHollowAtStart: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    thumbColor: {
+      type: String,
+      required: false
     },
     value: {
       type: Number,
@@ -87,19 +148,26 @@ export default Vue.extend({
       right: 8;
     }
     font-size: $font-sz-m;
+    font-weight: 700;
+    color: $primary-variant
+  }
+  .customSlider_icon {
+    vertical-align: bottom;
   }
   .customSlider_minValue {
     padding {
       left: 24;
     }
-    font-size: $font-sz-m;
+    font-size: $font-sz-s;
     text-align: left;
+    color: $primary
   }
   .customSlider_maxValue {
     padding {
       right: 24;
     }
-    font-size: $font-sz-m;
+    font-size: $font-sz-s;
     text-align: right;
+    color: $primary
   }
 </style>
