7261f957bc2489d1cec6f9d51d7456af1c1cbb60
Ignacio <ilopez-amor@ya.com>
2020-12-16 15:24:58 +0100

    Added delete confirm dialog

--- app/components/Dialogs/ConfirmDeleteLocation.ts
+++ app/components/Dialogs/ConfirmDeleteLocation.ts
@@ -1,6 +1,18 @@
-import confirmAlert from './ConfirmAlert'
+// import confirmDialog from './ConfirmDialog'
+import { confirm, ConfirmOptions } from '@nativescript/core'
 import { deleteLocation } from '@/store/locationsStore'
-import { ConfirmDialog } from './types'
 
-export const confirmDeleteLocation = (confirmDialog: ConfirmDialog, id: string): void =>
-  confirmAlert(confirmDialog, deleteLocation(id))
+export const confirmDeleteLocation = (options: ConfirmOptions, id: string): void => {
+  const confirmOptions: ConfirmOptions = {
+    title: options.title,
+    message: options.message,
+    okButtonText: options.okButtonText,
+    cancelButtonText: options.cancelButtonText,
+  }
+  confirm(confirmOptions).then((result) => {
+    if (result) deleteLocation(id)
+  })
+}
+
+// export const confirmDeleteLocation = (options: ConfirmDialog, id: string): void =>
+//   confirmDialog(options, deleteLocation(id))
