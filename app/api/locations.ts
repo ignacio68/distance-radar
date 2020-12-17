727b8aa6001ddc720e2ab5117743ba0ef9aa59a6
Ignacio <ilopez-amor@ya.com>
2020-12-17 17:56:33 +0100

    Refactored

--- app/api/locations.ts
+++ app/api/locations.ts
@@ -1,9 +1,12 @@
+import * as map from '@/services/mapboxService'
+
 import { setVisibility } from '@/composables/useComponent'
 
-import { getMap as map } from '@/store/mapStore'
+import { getMap } from '@/store/mapStore'
 import {
   addNewLocation,
   updateLocationsStore,
+  getLocation,
   getLocations,
   deleteLocation,
   deleteAllLocations,
@@ -13,12 +16,27 @@ import {
 } from '@/store/locationsStore'
 import { getUserMarker as userMarker } from '@/store/userMarkerStore'
 
-import { Location } from '@/types/types'
+import { Location, SecurityArea } from '@/types/types'
 
 export const showLocations = getLocations()
 
+const setLocationOpts = (location: Location): Location => {
+  const opts: Location = {
+    id: location.id,
+    lat: userMarker().lat,
+    lng: userMarker().lng,
+    title: location.id,
+    selected: true,
+    securityAreas: [],
+    onTap: () => onTap(opts.title),
+    onCalloutTap: () => onCalloutTap(),
+  }
+  const completeLocation: Location = { ...location, ...opts }
+  return completeLocation
+}
+
 const onTap = (id: string): void => {
-  console.log(`locations::onTap()`)
+  console.log(`locations::onTap(): ${id}`)
   hasSecurityArea(id).then((result) => {
     if (result) {
       console.log('The location has a security area')
@@ -32,28 +50,17 @@ const onTap = (id: string): void => {
 const onCalloutTap = (): void => console.log(`locations.ts::onCalloutTapLocation()`)
 
 export const newLocation = (location: Location): void => {
-  const opts = {
-    lat: userMarker().lat,
-    lng: userMarker().lng,
-    title: location.id,
-    selected: true,
-    hasSecurityArea: false,
-    onTap: () => onTap(location.id),
-    onCalloutTap: () => onCalloutTap(),
-  }
-  const completeLocation: Location = { ...location, ...opts }
+  const completeLocation: Location = setLocationOpts(location)
   console.log(`locations.ts::addLocation(): ${JSON.stringify(completeLocation)}`)
   const newLocation: Location[] = []
   newLocation.push(completeLocation)
-  map()
-    .addMarkers(newLocation)
-    .then(() => {
-      addNewLocation(completeLocation)
-    })
+  map.addMarkers(getMap(), newLocation).then(() => {
+    addNewLocation(completeLocation)
+  })
 }
 
 // export const fetchLocations = (): Locations[] => {
-//   map().addMarkers(location)
+//   map.addMarkers(getMap(), location)
 // }
 
 export const fetchSelectedLocation = (): Location => {
@@ -67,13 +74,10 @@ export const updateLocation = async (location: Location): Promise<void> => {
   })
 }
 
-export const updateLocationAtInit = async (location: Location): Promise<void> => {
-  const options: Record<string, unknown> = {
-    onTap: () => onTap(location.id),
-    onCalloutTap: () => onCalloutTap(),
-  }
-  const updatedLocation: Location = { ...location, ...options }
-  await updateLocation(updatedLocation)
+export const addSecurityAreaToLocation = (id: string, securityArea: SecurityArea): void => {
+  const location: Location = getLocation(id)
+  location.securityAreas.push(securityArea)
+  updateLocationsStore(location)
 }
 
 export const removeLocation = (id: string): void => {
@@ -84,4 +88,15 @@ export const removeLocation = (id: string): void => {
 export const removeAllLocations = (): void => {
   deleteAllLocations()
   console.log(`locations.ts::removeAllLocation:`)
+}
+
+/**************************************/
+
+export const updateLocationAtInit = async (location: Location): Promise<void> => {
+  const options: Record<string, unknown> = {
+    onTap: () => onTap(location.id),
+    onCalloutTap: () => onCalloutTap(),
+  }
+  const updatedLocation: Location = { ...location, ...options }
+  await updateLocation(updatedLocation)
 }
