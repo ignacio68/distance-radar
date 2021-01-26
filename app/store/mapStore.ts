19ca733a6641b93f0fe60bbe97b21d6bdd0d81e2
Ignacio <ilopez-amor@ya.com>
2021-01-26 13:50:05 +0100

    Added persistent storage

--- app/store/mapStore.ts
+++ app/store/mapStore.ts
@@ -1,12 +1,35 @@
 import Vue from 'nativescript-vue'
+
+import { createDatabase, getItem, addItem, updateItem, resetDatabase } from '@/api/storage'
+
+import { Database } from '@/api/types'
 import { Map } from '@/services/types'
 
 const state = Vue.observable({
   map: null as Map,
 })
 
+// Create persist locations database
+const database: Database = createDatabase('map')
+
+const initializeDatabase = (): void => {
+  const map: Map = getItem(database, 'map')
+
+  map && setMap(map)
+}
+
+// initializeDatabase()
+
 export const getMap = (): Map => state.map
 
 export const setMap = (map: Map): void => {
   state.map = map
+
+  const isMap: Map = getItem(database, 'map')
+  isMap ? updateItem(database, 'map', map) : addItem<Map>(database, map, 'map')
+}
+
+export const deleteMap = (): void => {
+  state.map = null
+  resetDatabase(database)
 }
