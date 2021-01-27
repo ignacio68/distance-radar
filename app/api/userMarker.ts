bb026f421c3d0862c06889accf09abff58ac614b
Ignacio <ilopez-amor@ya.com>
2021-01-27 12:39:31 +0100

    Remove consts

--- app/api/userMarker.ts
+++ app/api/userMarker.ts
@@ -1,27 +1,25 @@
 // import * as map from '@/services/mapboxService'
 import { mbAddMarkers } from '@/services/mapboxService'
 
-import { getMap } from '@/store/mapStore'
+import { getMap as map } from '@/store/mapStore'
 import {
   setUserMarker,
   getUserMarker as userMarker,
   updateUserMarkerPosition,
 } from '@/store/userMarkerStore'
 import { getCurrentUserLocation as userLocation } from '@/store/userLocationStore'
+
 import { setVisibility } from '@/composables/useComponent'
 
 import { UserMarker, LatLng } from '@/types/commons'
 
-const map = getMap()
 
 const onTap = (): boolean => setVisibility('newLocationMenu', true)
 
-const isUserMarker = async (coordinates: LatLng): Promise<void> => {
-  Promise.resolve(!userMarker() ? createUserMarker(coordinates) : console.log('User marker exits!'))
-}
+const isUserMarker = (): boolean => userMarker() ? true : false
 
 export const createUserMarker = (coordinates?: LatLng): void => {
-  console.log('addMarker()')
+  console.log('createUserMarker()')
   const initialValues: UserMarker = {
     id: '_user',
     lat: coordinates.lat || userLocation().lat,
@@ -29,23 +27,21 @@ export const createUserMarker = (coordinates?: LatLng): void => {
     onTap: () => onTap(),
   }
   const newUserMarker = [initialValues]
-  mbAddMarkers(map, newUserMarker).then(() => setUserMarker(initialValues))
+  mbAddMarkers(map(), newUserMarker).then(() => setUserMarker(initialValues))
 }
 
-export const updateUserMarker = async (coordinates: LatLng): Promise<void> => {
+export const updateUserMarker = (coordinates: LatLng): void => {
   console.log('updateUserMarker()')
-  await isUserMarker(coordinates).then(() => {
+  if (isUserMarker()) {
     const values = {
       id: '_user',
       lat: coordinates.lat,
       lng: coordinates.lng,
     }
-    const currentUserMarker: UserMarker = userMarker()
-    if (currentUserMarker) {
-      currentUserMarker.update(values)
-      updateUserMarkerPosition(coordinates)
-    } else {
-      console.log('There is not user marker')
-    }
-  })
+    userMarker().update(values)
+    updateUserMarkerPosition(coordinates)
+  }
+  else {
+    createUserMarker(coordinates)
+  }
 }
