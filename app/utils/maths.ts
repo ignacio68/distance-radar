ef28a52718a1083477dc866a63eef31c6cd50f9f
Ignacio <ilopez-amor@ya.com>
2021-02-04 14:19:32 +0100

    Remove math file

--- app/utils/maths.ts
+++
@@ -1,8 +0,0 @@
-const TO_RADIANS = 0.017453292519943295 // (PI / 180)
-const TO_DEGREES = 57.2958 // (180 / PI)
-
-export const range = (start: number, stop: number, step: number): number[] =>
-  Array.from({ length: (stop - start) / step + 1 }, (_, i) => start + i * step)
-
-export const toRadians = (degrees: number): number => degrees * TO_RADIANS
-export const toDegrees = (radians: number): number => radians * TO_DEGREES
