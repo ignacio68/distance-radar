8bf4355a173700f89e596aa7a2b9adf8e911c522
Ignacio <ilopez-amor@ya.com>
2021-02-04 14:15:40 +0100

    Clean code

--- app/api/securityAreas/layer.ts
+++ app/api/securityAreas/layer.ts
@@ -1,32 +1,38 @@
 import { createSource } from './source'
-import { SecurityAreaOptions, PolygonLayerStyleOptions, PolygonLayer } from '@/api/types'
+import { SecurityAreaOptions, PolygonLayerStyleOptions, PolygonLayer, Source } from '@/api/types'
 
-export const createLayer = (options: SecurityAreaOptions): PolygonLayer =>
-  getGeoJSONLayerOptions(options)
+export const createLayer = (options: SecurityAreaOptions): PolygonLayer => setLayerOptions(options)
 
-const getGeoJSONLayerOptions = (options: SecurityAreaOptions): PolygonLayer => {
+const setLayerOptions = (options: SecurityAreaOptions): PolygonLayer => {
   const { id } = options
   const layerId = getLayerId(id)
   const source = createSource(id, options)
   const style = getLayerStyle(options)
-  const layerOptions: PolygonLayer = {
-    id: layerId,
-    sourceLayer: source,
-    ...style,
-  }
+  const layerOptions = getLayerOptions(layerId, source, style)
+
   return layerOptions
 }
 
 const getLayerId = (id: string): string => `${id}_layer`
 
 const getLayerStyle = (args: SecurityAreaOptions): PolygonLayerStyleOptions => {
-  const { fillColor, fillOpacity, isVisible } = args
+  const { fillColor, fillOpacity, visibility } = args
   const style: PolygonLayerStyleOptions = {
-    fillAntialias: true,
-    fillColor,
-    fillOpacity,
-    fillOutlineColor: fillColor,
-    visibility: isVisible,
+    'fill-color': fillColor,
+    'fill-opacity': fillOpacity,
+    visibility,
   }
   return style
 }
+
+const getLayerOptions = (
+  layerId: string,
+  source: Source,
+  style: PolygonLayerStyleOptions,
+): PolygonLayer => ({
+  id: layerId,
+  type: 'fill',
+  source,
+  layout: {},
+  paint: style,
+})
