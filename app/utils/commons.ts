ef4ad03bf6c5bd96169e4aef124c625571889ceb
Ignacio <ilopez-amor@ya.com>
2021-02-11 11:33:30 +0100

    Added color validation

--- app/utils/commons.ts
+++ app/utils/commons.ts
@@ -1,6 +1,9 @@
+import validateColor from 'validate-color'
 import { LatLng, Position } from '@/types/commons'
 
 export const latLngToPosition = (coordinates: LatLng): Position => [
   coordinates.lng,
   coordinates.lat,
 ]
+
+export const isColor = (color: string): boolean => (color && validateColor(color) ? true : false)
