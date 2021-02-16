95ebcb6bebcc3a1b9a37cf0d75fccf1f9ac55ef7
Ignacio <ilopez-amor@ya.com>
2021-02-16 14:17:27 +0100

    Deleted unused function

--- app/store/userMarkerStore.ts
+++ app/store/userMarkerStore.ts
@@ -6,11 +6,11 @@ const state = Vue.observable({
   userMarker: [],
 })
 
-export const hasUserMarker = (): boolean => state.userMarker.length > 0
-
 export const getUserMarker = (): UserMarker => state.userMarker[0]
 
-export const setUserMarker = (marker: UserMarker): number => state.userMarker.push(marker)
+export const setUserMarker = (marker: UserMarker): void => {
+  state.userMarker.push(marker)
+}
 
 export const updateUserMarkerPosition = (coordinates: LatLng): void => {
   state.userMarker[0].lat = coordinates.lat
