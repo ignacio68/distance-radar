2a26cadc62912fd6980519e4b956ff1e1d7f3194
Ignacio <ilopez-amor@ya.com>
2021-03-02 19:25:51 +0100

    improved text test code

--- app/tests/unit/utils/text.test.ts
+++ app/tests/unit/utils/text.test.ts
@@ -1,22 +1,7 @@
-import { removeEmptyKeys, objectToArray, reduceToString } from '@/utils/text'
+import { objectToArray, removeEmptyStrings, reduceToString } from '@/utils/text'
 import { assert, expect } from 'chai'
 
 describe.skip('Utils - Text', function () {
-  describe('#removeEmptyKeys', function () {
-    describe('remove the empty keys of an object', function () {
-      it('return an object without the empty keys', function () {
-        const fakeObject = {
-          1: 'a',
-          2: '',
-        }
-
-        const cleanObject = removeEmptyKeys(fakeObject)
-
-        assert.deepEqual({ 1: 'a' }, cleanObject)
-      })
-    })
-  })
-
   describe('#objectToArray', function () {
     describe('convert an object to an array', function () {
       it('return an array', function () {
@@ -28,6 +13,18 @@ describe.skip('Utils - Text', function () {
 
         expect(arrayFromObject).to.be.an.instanceOf(Array, 'it is not an array')
         expect(arrayFromObject).to.be.deep.equal(['a'])
+      })
+    })
+  })
+
+  describe('#removeEmptyKeys', function () {
+    describe('remove the empty keys of an object', function () {
+      it('return an object without the empty keys', function () {
+        const fakeArray = ['a', '']
+
+        const cleanArray = removeEmptyStrings(fakeArray)
+
+        assert.deepEqual(['a'], cleanArray)
       })
     })
   })
