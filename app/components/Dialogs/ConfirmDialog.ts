b798ef5ac01b4b816ec681c8d07efbf836629c0f
Ignacio <ilopez-amor@ya.com>
2021-03-11 17:38:48 +0100

    improved code

--- app/components/Dialogs/ConfirmDialog.ts
+++ app/components/Dialogs/ConfirmDialog.ts
@@ -1,17 +1,17 @@
 import { confirm, ConfirmOptions } from '@nativescript/core'
 import { ConfirmDialog } from './types'
 
-const confirmDialog = (options: ConfirmDialog, callback: () => {}): void => {
-  const confirmOptions: ConfirmOptions = {
-    title: options.title,
-    message: options.message,
-    okButtonText: options.okButtonText,
-    cancelButtonText: options.cancelButtonText,
-  }
-  confirm(confirmOptions).then((result) => {
-    console.log(`ConfirmDialog::result: ${result}`)
-    if (result) callback
+export const loadConfirmDialog = (options: ConfirmDialog) => {
+  const confirmOptions = setConfirmDialogOptions(options)
+  confirm(confirmOptions).then(() => {
+    console.log(`ConfirmDialog}`)
+    options.callback
   })
 }
 
-export default confirmDialog
+const setConfirmDialogOptions = (options: ConfirmOptions): ConfirmOptions => ({
+  title: options.title,
+  message: options.message,
+  okButtonText: options.okButtonText,
+  cancelButtonText: options.cancelButtonText,
+})
