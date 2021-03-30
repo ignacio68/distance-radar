ab258e893be8b40ae3d1604b8eb8d843166d5d02
Ignacio <ilopez-amor@ya.com>
2021-03-30 11:54:58 +0200

    Added textfield toprint  the value of the slider

--- app/components/UI/CustomSlider.vue
+++ app/components/UI/CustomSlider.vue
@@ -1,73 +1,76 @@
 <template>
-  <GridLayout class="CustomSlider" columns="auto,*" :width="sliderWidth">
-    <!-- <Icon
-      col="0"
-      class="CustomSlider__icon"
-      :size="iconSize"
-      :name="iconName"
-      :iconColor="iconColor"
-      :hasRipple="false"
-    /> -->
-    <StackLayout col="1">
-      <Label class="CustomSlider__name" :text="sliderName" />
-      <GridLayout columns="auto, *, auto" class="m-x-32 m-b-0">
-        <Label col="0" color="black" class="CustomSlider__minValue" :text="minValue" />
-        <StackLayout col="1" width="100%" color="red"></StackLayout>
-        <Label col="2" class="CustomSlider__maxValue" :text="maxValue" />
-      </GridLayout>
-      <MDSlider
-        :value="value"
-        col="0"
-        row="0"
-        colSpan="3"
-        class="CustomSlider__slider m-x-32 m-t-0"
-        :minValue="minValue"
-        :maxValue="maxValue"
-        :thumbHollowAtStart="thumbHollowAtStart"
-        :thumbColor="thumbColor"
-        :rippleColor="rippleColor"
-        :elevation="elevation"
-        @valueChange="onValueChanged"
+  <StackLayout class="CustomSlider">
+    <GridLayout columns="auto, auto, auto">
+      <Label class="CustomSlider__name p-t-16" col="0" :text="sliderName" />
+      <TextField
+        ref="textField"
+        class="CustomSlider__textField p-l-32"
+        col="1"
+        :width="textFieldWidth"
+        verticalAlignment="center"
+        borderColor="white"
+        :isEnabled="true"
+        :editable="false"
+        keyboardType="number"
+        returnKeyType="done"
+        :maxLength="4"
+        :text="value"
       />
-    </StackLayout>
-  </GridLayout>
+      <Label
+        v-if="isVisibleUnitsLabel"
+        class="CustomSlider__units p-t-16 m-l-0"
+        col="2"
+        :text="units"
+      />
+    </GridLayout>
+    <GridLayout columns="auto, *, auto" class="m-x-32 m-b-0">
+      <Label col="0" color="black" class="CustomSlider__minValue" :text="minValue" />
+      <StackLayout col="1" width="100%" color="red"></StackLayout>
+      <Label col="2" class="CustomSlider__maxValue" :text="maxValue" />
+    </GridLayout>
+    <MDSlider
+      :value="value"
+      col="0"
+      row="0"
+      colSpan="3"
+      class="CustomSlider__slider m-x-32 m-t-0"
+      :minValue="minValue"
+      :maxValue="maxValue"
+      :thumbHollowAtStart="thumbHollowAtStart"
+      :thumbColor="thumbColor"
+      :rippleColor="rippleColor"
+      :elevation="elevation"
+      @valueChange="onValueChanged"
+    />
+  </StackLayout>
 </template>
 
 <script lang="ts">
 import Vue from 'nativescript-vue'
 
-// import Icon from '@/components/UI/Icon.vue'
-
 export default Vue.extend({
   name: 'CustomSlider',
-  // components: { Icon },
+
   props: {
-    // Icon.$props,
-    sliderWidth: {
-      type: [String, Number],
-      default: '100%',
-    },
-
-    // iconName: {
-    //   type: String,
-    //   default: '',
-    // },
-    // iconSize: {
-    //   type: [String, Number],
-    //   default: '48',
-    // },
-    // iconColor: {
-    //   type: String,
-    //   default: 'black',
-    // },
-    rippleColor: {
-      type: String,
-      required: false,
-    },
-
     sliderName: {
       type: String,
       default: '',
+    },
+    textFieldWidth: {
+      type: [String, Number],
+      default: '100%',
+    },
+    isVisibleUnitsLabel: {
+      type: Boolean,
+      default: true,
+    },
+    units: {
+      type: String,
+      default: '',
+    },
+    rippleColor: {
+      type: String,
+      required: false,
     },
     minValue: {
       type: Number,
@@ -96,11 +99,17 @@ export default Vue.extend({
     },
   },
   data() {
-    return {
-      // value: 0
-    }
+    return {}
   },
   methods: {
+    onReturnPress() {
+      this.$emit('on-return-press', this.textFieldValue)
+    },
+
+    onTextChange() {
+      console.log(`TextForm::"value: ${this.textFieldValue}"`)
+    },
+
     onValueChanged({ value }) {
       this.$emit('on-value-changed', value)
       console.log(`CustomSlider::onValueChanged():value: ${value}`)
@@ -120,9 +129,21 @@ export default Vue.extend({
   font-weight: 700;
   color: $primary-variant;
 }
-// .CustomSlider__icon {
-//   vertical-align: bottom;
-// }
+.CustomSlider__units {
+  font-size: $font-sz-m;
+  font-weight: 400;
+  color: $primary-variant;
+}
+.CustomSlider__textField {
+  border-radius: 4;
+  background-color: rgba($primary, 0.08);
+  &[text] {
+    padding-left: 8;
+    color: $primary-variant;
+    vertical-align: center;
+    font-size: $font-sz-m;
+  }
+}
 .CustomSlider__minValue {
   padding {
     left: 24;
