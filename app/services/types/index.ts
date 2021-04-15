1bcc9f36e67fe4cd39685cf33f34ec0981a71ce1
Ignacio <ilopez-amor@ya.com>
2021-04-15 11:24:33 +0200

    Changed export/import types

--- app/services/types/index.ts
+++ app/services/types/index.ts
@@ -1,5 +1,6 @@
-export { LatLng } from '@/types/commons'
-export { CouchBase as Database, Query } from '@triniwiz/nativescript-couchbase'
+import { LatLng } from '@/types/commons'
+import { CouchBase as Database, Query } from '@triniwiz/nativescript-couchbase'
+export { LatLng, Database, Query }
 export * from './geocoding'
 export * from './circle'
 export * from './mapbox'
--- app/services/types/mapbox.ts
+++ app/services/types/mapbox.ts
@@ -1,10 +1,11 @@
 import { LatLng } from '.'
-export {
+import {
   MapboxApi as Map,
   SetViewportOptions,
   ShowOptions,
   Bounds,
 } from '@nativescript-community/ui-mapbox'
+export { Map, SetViewportOptions, ShowOptions, Bounds }
 export interface SetOnMapLongClickListener {
   (data: LatLng): boolean
 }
