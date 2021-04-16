05b83043b33fa0f0320aeec67aae0418c42866c7
Ignacio <ilopez-amor@ya.com>
2021-04-16 10:30:44 +0200

    Remove unused type and changed plugin source

--- app/services/geocodingService.ts
+++ app/services/geocodingService.ts
@@ -1,12 +1,12 @@
-import * as geocoding from 'nativescript-geocoding'
+import * as geocoding from '@nativescript-community/geocoding'
 
-import { GeocoderLocation, SearchLocationsResult } from './types/geocoding'
+import { SearchLocationsResult } from './types/geocoding'
 
 export const searchLocations = async (query: string): Promise<SearchLocationsResult> => {
   const fetchLocations = await geocoding.getLocationListFromName(query, 5).then(
     (locations: SearchLocationsResult) => locations,
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (error: any) => console.log(`Error: ${error.message || error}`)
+    (error: any) => console.log(`Error: ${error.message || error}`),
   )
   console.log(`fetchLocations: ${JSON.stringify(fetchLocations)}`)
   return fetchLocations
